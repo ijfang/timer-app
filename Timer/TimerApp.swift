cb43407bf5f3e0dbaa9a478878844c691729b968
Isabelle Fang <ijfang19@gmail.com>
2025-09-09 21:41:55 -0400

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Isabelle Fang on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimerTests/TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTests.swift
+//  TimerTests
+//
+//  Created by Isabelle Fang on 9/9/25.
+//
+
+import Testing
+@testable import Timer
+
+struct TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
